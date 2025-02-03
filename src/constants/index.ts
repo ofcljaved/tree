3efc71aea9f98fcbae8c3489558dd960c9b074a7
ofcljaved <javedofficialme@gmail.com>
2025-02-03 18:11:52 +0530

    feat: add exact character and dir count

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+//.
+//├── 123
+//├── ABCD
+//├── AbC
+//│   └── 1243
+//├── aBc.txt
+//├── abcdefg
+//│   └── efg
+//│       └── hij.txt
+//└── abcs
+
+export const SINGLE_LINE = "└";
+export const DOUBLE_LINE = "├";
+export const TREE = "│";
+export const SPACE = "── ";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import type { Dirent } from "node:fs";
 import fs from "node:fs/promises";
+import sortTree from "./utils/sort-tree";
+import isHidden from "./utils/hidden";
+import { DOUBLE_LINE, SINGLE_LINE, SPACE, TREE } from "./constants";
 
 //.
 //├── 123
@@ -12,48 +14,66 @@ import fs from "node:fs/promises";
 //│       └── hij.txt
 //└── abcs
 
-function sortTree(arrOfDirent: Dirent[]) {
-  return arrOfDirent.toSorted((a, b) => {
-    if (a.isFile() || b.isFile()) {
-      if (a.isFile() && !b.isFile()) return 1;
-      if (!a.isFile() && b.isFile()) return -1;
-      return 0;
-    } else {
-      return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
-    }
-  });
-}
-
-function isHidden(filename: string) {
-  return /^\./.test(filename);
-}
+type FileTree = {
+  dirCount: number;
+  fileCount: number;
+  subTree: string;
+};
 
 const padding = "   ";
-async function tree(path: string, indent: number = 0) {
+async function tree(path: string, indent: number = 0): Promise<FileTree> {
   const listOfDir = await fs.readdir(path, { withFileTypes: true });
   const sortedDir = sortTree(listOfDir);
-  let test = "";
-  for (const dir of sortedDir) {
+  const len = sortedDir.length;
+
+  if (len === 0) return { dirCount: 0, fileCount: 0, subTree: "" };
+
+  let subTree = "";
+  let dirCount = 0;
+  let fileCount = 0;
+
+  for (let i = 0; i < len; i++) {
+    const dir = sortedDir[i];
     if (isHidden(dir.name)) continue;
-    test += padding.repeat(indent);
-    test += `|--${dir.name}\n`;
+    subTree += padding.repeat(indent);
+
+    const isLast = i === len - 1;
+    const prefix = isLast ? SINGLE_LINE : DOUBLE_LINE;
+
+    subTree += `${prefix}${SPACE}${dir.name}\n`;
+
     if (dir.isDirectory()) {
+      dirCount++;
       const res = await tree(path + "/" + dir.name, indent + 1);
-      if (res.length) {
-        test += res;
+      if (res.subTree.length) {
+        subTree += TREE;
+        subTree += res.subTree;
+        dirCount += res.dirCount;
+        fileCount += res.fileCount;
       }
+    } else {
+      console.log(dir.name, fileCount);
+      fileCount++;
+      console.log(fileCount);
     }
   }
-  return test;
+  return {
+    dirCount,
+    fileCount,
+    subTree,
+  };
 }
 
 async function main() {
   const path = "./test";
   try {
     let fileTree = path;
-    let subTree = await tree(path);
-    fileTree += `\n${subTree}`;
-    console.log(subTree);
+    let res = await tree(path);
+    fileTree += `\n${res.subTree}`;
+    let dirCount = res.dirCount === 0 && res.fileCount === 0 ? res.dirCount : res.dirCount + 1;
+    let fileCount = res.fileCount;
+    console.log(fileTree);
+    console.log(`${dirCount} directories, ${fileCount} files`);
   } catch (e) {
     console.error(e);
   }
